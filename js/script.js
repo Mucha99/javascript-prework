6d1c3f7a45906b619292909246be4f272a001224
Bartek Mucha <bartekmucha43@gmail.com>
2021-02-08 19:33:09 +0100

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-function printMessage(msg) {
-  let div = document.createElement("div");
-  div.innerHTML = msg;
-  document.getElementById("messages").appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log("Wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else if (randomNumber == 3) {
+  computerMove = "nożyczki";
 }
 
-function clearMessages() {
-  document.getElementById("messages").innerHTML = "";
-}
-
-let computerMove = "kamień";
-let playerMove = "papier";
-
-printMessage(
-  "Zagrałem " +
-    computerMove +
-    "! Jeśli Twój ruch to " +
-    playerMove +
-    ", to wygrywasz!"
-);
+printMessage("Mój ruch to: " + computerMove);
